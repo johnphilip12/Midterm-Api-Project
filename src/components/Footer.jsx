505d5de740f37dd257ec2d8bf82c99ad0b13e98c
John Philip Villamor <villamorjohnphilip2@gmail.com>
2022-10-16 20:30:12 +0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return ( 
+        <div className="footer bg-secondary text-light">
+
+        Copyright &copy;<b>Villamor John Philip</b>.
+            &nbsp; All rights reserved. 
+       
+        </div>
+     );
+}
+ 
+export default Footer;
